087098ace92f9cf6135e3d155c322ec5b3d28fd3
Bibek Dahal <bibekdahal_bd16@hotmail.com>
2015-08-19 11:49:11 +0545

    Create a basic question editor

---
+++ IoE Model Entrance/Question Editor/QuestionsEditor.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question_Editor
+{
+    public partial class QuestionsEditor : Form
+    {
+        public QuestionsEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void QuestionsEditor_Load(object sender, EventArgs e)
+        {
+            QuestionControls controls = new QuestionControls();
+            controls.label = lbl_qn1; controls.question = rtb_question1;
+            controls.optiona = rtb_optiona1; controls.optionb = rtb_optionb1; 
+            controls.optionc = rtb_optionc1; controls.optiond = rtb_optiond1;
+            m_questionControls.Add(controls);
+            m_questions.Add(new Question());
+
+            int distance = 370;
+            int shift = distance;
+            for (int i = 1; i < m_questionsPerPage; ++i)
+            {
+                Label label = lbl_qn1.Clone();
+                RichTextBox question = rtb_question1.Clone();
+                RichTextBox optiona = rtb_optiona1.Clone();
+                RichTextBox optionb = rtb_optionb1.Clone();
+                RichTextBox optionc = rtb_optionc1.Clone();
+                RichTextBox optiond = rtb_optiond1.Clone();
+
+                label.Top += shift;
+                question.Top += shift;
+                optiona.Top += shift;
+                optionb.Top += shift;
+                optionc.Top += shift;
+                optiond.Top += shift;
+
+                question.Tag = "qn" + i;
+                optiona.Tag = "oa" + i;
+                optionb.Tag = "ob" + i;
+                optionc.Tag = "oc" + i;
+                optiond.Tag = "od" + i;
+
+                shift += distance;
+
+                controls = new QuestionControls();
+                controls.label = label; controls.question = question;
+                controls.optiona = optiona; controls.optionb = optionb; controls.optionc = optionc; controls.optiond = optiond;
+                m_questionControls.Add(controls);
+                m_questions.Add(new Question());
+            }
+            RefreshQuestions();
+
+            foreach (QuestionControls c in m_questionControls)
+            {
+                c.question.TextChanged += new EventHandler(text_changed);
+                c.optiona.TextChanged += new EventHandler(text_changed); 
+                c.optionb.TextChanged += new EventHandler(text_changed); 
+                c.optionc.TextChanged += new EventHandler(text_changed); 
+                c.optiond.TextChanged += new EventHandler(text_changed); 
+            }
+        }
+
+
+        private int m_currentPage = 0;
+        private int m_totalPages = 1;
+        private int m_questionsPerPage = 10;
+
+        struct QuestionControls {
+            public Label label;
+            public RichTextBox question, optiona, optionb, optionc, optiond;
+        }
+
+        private class Question {
+            public String question="", optiona="", optionb="", optionc="", optiond="";
+        }
+
+        private List<QuestionControls> m_questionControls =  new List<QuestionControls>();
+        private List<Question> m_questions = new List<Question>();
+
+        public void RefreshQuestions()
+        {
+            int i = m_currentPage * m_questionsPerPage;
+            foreach (QuestionControls qc in m_questionControls) 
+            {
+                Question q =  m_questions[i];
+                qc.label.Text = (i+1) + ".";
+                qc.question.Rtf = q.question;
+                qc.optiona.Rtf = q.optiona;
+                qc.optionb.Rtf = q.optionb;
+                qc.optionc.Rtf = q.optionc;
+                qc.optiond.Rtf = q.optiond;
+                i++;
+            }
+        }
+
+        private void btn_prev_Click(object sender, EventArgs e)
+        {
+            if (m_currentPage == 0)
+                return;
+            m_currentPage--;
+            RefreshQuestions();
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            m_currentPage++;
+            if (m_currentPage >= m_totalPages)
+            {
+                m_totalPages++;
+                for (int i = 0; i < m_questionsPerPage; ++i)
+                    m_questions.Add(new Question());
+            }
+            RefreshQuestions();
+        }
+
+        public void text_changed(object _sender, EventArgs args)
+        {
+            RichTextBox sender = (RichTextBox)_sender;
+            String tag = sender.Tag.ToString();
+            String type = tag.Substring(0, 2);
+            int index = Int32.Parse(tag.Substring(2));
+
+            int i = m_currentPage * m_questionsPerPage;
+            if (type == "qn")
+                m_questions[i + index].question = sender.Rtf;
+            else if (type == "oa")
+                m_questions[i + index].optiona = sender.Rtf;
+            else if (type == "ob")
+                m_questions[i + index].optionb = sender.Rtf;
+            else if (type == "oc")
+                m_questions[i + index].optionc = sender.Rtf;
+            else if (type == "od")
+                m_questions[i + index].optiond = sender.Rtf;
+        }
+    }
+}
---
+++ IoE Model Entrance/Question Editor/QuestionsEditor.designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Question_Editor
+{
+    partial class QuestionsEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_holder = new System.Windows.Forms.Panel();
+            this.lbl_qn1 = new System.Windows.Forms.Label();
+            this.rtb_optionb1 = new System.Windows.Forms.RichTextBox();
+            this.rtb_optiond1 = new System.Windows.Forms.RichTextBox();
+            this.rtb_optiona1 = new System.Windows.Forms.RichTextBox();
+            this.rtb_question1 = new System.Windows.Forms.RichTextBox();
+            this.rtb_optionc1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.pnl_holder.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_holder
+            // 
+            this.pnl_holder.AutoScroll = true;
+            this.pnl_holder.Controls.Add(this.lbl_qn1);
+            this.pnl_holder.Controls.Add(this.rtb_optionb1);
+            this.pnl_holder.Controls.Add(this.rtb_optiond1);
+            this.pnl_holder.Controls.Add(this.rtb_optiona1);
+            this.pnl_holder.Controls.Add(this.rtb_question1);
+            this.pnl_holder.Controls.Add(this.rtb_optionc1);
+            this.pnl_holder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_holder.Location = new System.Drawing.Point(0, 0);
+            this.pnl_holder.Name = "pnl_holder";
+            this.pnl_holder.Size = new System.Drawing.Size(872, 446);
+            this.pnl_holder.TabIndex = 2;
+            // 
+            // lbl_qn1
+            // 
+            this.lbl_qn1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbl_qn1.AutoSize = true;
+            this.lbl_qn1.Location = new System.Drawing.Point(20, 11);
+            this.lbl_qn1.Name = "lbl_qn1";
+            this.lbl_qn1.Size = new System.Drawing.Size(16, 13);
+            this.lbl_qn1.TabIndex = 1;
+            this.lbl_qn1.Text = "1.";
+            // 
+            // rtb_optionb1
+            // 
+            this.rtb_optionb1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_optionb1.Location = new System.Drawing.Point(447, 182);
+            this.rtb_optionb1.Name = "rtb_optionb1";
+            this.rtb_optionb1.Size = new System.Drawing.Size(385, 69);
+            this.rtb_optionb1.TabIndex = 0;
+            this.rtb_optionb1.Tag = "ob0";
+            this.rtb_optionb1.Text = "";
+            // 
+            // rtb_optiond1
+            // 
+            this.rtb_optiond1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_optiond1.Location = new System.Drawing.Point(447, 257);
+            this.rtb_optiond1.Name = "rtb_optiond1";
+            this.rtb_optiond1.Size = new System.Drawing.Size(385, 69);
+            this.rtb_optiond1.TabIndex = 0;
+            this.rtb_optiond1.Tag = "od0";
+            this.rtb_optiond1.Text = "";
+            // 
+            // rtb_optiona1
+            // 
+            this.rtb_optiona1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_optiona1.Location = new System.Drawing.Point(49, 182);
+            this.rtb_optiona1.Name = "rtb_optiona1";
+            this.rtb_optiona1.Size = new System.Drawing.Size(392, 69);
+            this.rtb_optiona1.TabIndex = 0;
+            this.rtb_optiona1.Tag = "oa0";
+            this.rtb_optiona1.Text = "";
+            // 
+            // rtb_question1
+            // 
+            this.rtb_question1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_question1.Location = new System.Drawing.Point(49, 12);
+            this.rtb_question1.Name = "rtb_question1";
+            this.rtb_question1.Size = new System.Drawing.Size(783, 164);
+            this.rtb_question1.TabIndex = 0;
+            this.rtb_question1.Tag = "qn0";
+            this.rtb_question1.Text = "";
+            // 
+            // rtb_optionc1
+            // 
+            this.rtb_optionc1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_optionc1.Location = new System.Drawing.Point(49, 257);
+            this.rtb_optionc1.Name = "rtb_optionc1";
+            this.rtb_optionc1.Size = new System.Drawing.Size(392, 69);
+            this.rtb_optionc1.TabIndex = 0;
+            this.rtb_optionc1.Tag = "oc0";
+            this.rtb_optionc1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_prev);
+            this.panel1.Controls.Add(this.btn_next);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 446);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(872, 52);
+            this.panel1.TabIndex = 2;
+            // 
+            // btn_prev
+            // 
+            this.btn_prev.Location = new System.Drawing.Point(49, 17);
+            this.btn_prev.Name = "btn_prev";
+            this.btn_prev.Size = new System.Drawing.Size(75, 23);
+            this.btn_prev.TabIndex = 0;
+            this.btn_prev.Text = "Previous";
+            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_next.Location = new System.Drawing.Point(732, 17);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(75, 23);
+            this.btn_next.TabIndex = 0;
+            this.btn_next.Text = "Next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // QuestionsEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 498);
+            this.Controls.Add(this.pnl_holder);
+            this.Controls.Add(this.panel1);
+            this.Name = "QuestionsEditor";
+            this.Text = "Questons Editor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.QuestionsEditor_Load);
+            this.pnl_holder.ResumeLayout(false);
+            this.pnl_holder.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_holder;
+        private System.Windows.Forms.Label lbl_qn1;
+        private System.Windows.Forms.RichTextBox rtb_optionb1;
+        private System.Windows.Forms.RichTextBox rtb_optiond1;
+        private System.Windows.Forms.RichTextBox rtb_optiona1;
+        private System.Windows.Forms.RichTextBox rtb_question1;
+        private System.Windows.Forms.RichTextBox rtb_optionc1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_prev;
+        private System.Windows.Forms.Button btn_next;
+    }
+}
+
