84201f3b70d949d3c4ed24fc593cf573e2679471
Bibek Dahal <bibekdahal_bd16@hotmail.com>
2015-08-20 21:17:57 +0545

    Add Timer

---
+++ IoE Model Entrance/Model Entrance/QuestionPage.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Model_Entrance
+{
+    public partial class QuestionPage : Form
+    {
+        public QuestionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void QuestionsEditor_Load(object sender, EventArgs e)
+        {
+            QuestionControls controls = new QuestionControls();
+            controls.label = lbl_qn1; controls.question = rtb_question1;
+            controls.optiona = rtb_optiona1; controls.optionb = rtb_optionb1; 
+            controls.optionc = rtb_optionc1; controls.optiond = rtb_optiond1;
+            controls.selecta = radio_a; controls.selectb = radio_b;
+            controls.selectc = radio_c; controls.selectd = radio_d;
+            controls.panel = pnl_question;
+            m_questionControls.Add(controls);
+            m_questions.Add(new Question());
+
+            for (int i = 1; i < m_questionsPerPage; ++i)
+            {
+                controls = new QuestionControls();
+                controls.label = lbl_qn1.Clone();
+                controls.question = rtb_question1.Clone();
+                controls.optiona = rtb_optiona1.Clone();
+                controls.optionb = rtb_optionb1.Clone();
+                controls.optionc = rtb_optionc1.Clone();
+                controls.optiond = rtb_optiond1.Clone();
+                controls.selecta = radio_a.Clone();
+                controls.selectb = radio_b.Clone();
+                controls.selectc = radio_c.Clone();
+                controls.selectd = radio_d.Clone();
+                controls.panel = pnl_question.Clone();
+
+                controls.ChangeParent();
+
+                controls.question.Tag = "qn" + i;
+                controls.optiona.Tag = "oa" + i;
+                controls.optionb.Tag = "ob" + i;
+                controls.optionc.Tag = "oc" + i;
+                controls.optiond.Tag = "od" + i;
+
+                m_questionControls.Add(controls);
+                m_questions.Add(new Question());
+            }
+
+            foreach (QuestionControls c in m_questionControls)
+            {
+
+                c.question.ContentsResized += new ContentsResizedEventHandler(content_resized);
+                c.optiona.ContentsResized += new ContentsResizedEventHandler(content_resized);
+                c.optionb.ContentsResized += new ContentsResizedEventHandler(content_resized);
+                c.optionc.ContentsResized += new ContentsResizedEventHandler(content_resized);
+                c.optiond.ContentsResized += new ContentsResizedEventHandler(content_resized);
+
+                c.question.Text = c.optiona.Text = c.optionb.Text = c.optionc.Text = c.optiond.Text = "\n";
+                c.question.Text = c.optiona.Text = c.optionb.Text = c.optionc.Text = c.optiond.Text = "";
+            }
+
+            RefreshQuestions();
+            RefreshControls();
+
+            m_timer = new System.Timers.Timer(1000);
+            m_timer.Elapsed += new System.Timers.ElapsedEventHandler(time_elapsed);
+            m_timer.SynchronizingObject = this;
+            m_timer.Start();
+        }
+
+        private int seconds = 0;
+        private int target_seconds = 3*60*60; // 3 hrs
+        public void time_elapsed(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            seconds++;
+            lbl_timer.Text = "Time Elapsed: " + TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm\:ss");
+
+            if (seconds == target_seconds)
+            {
+                m_timer.Stop();
+                MessageBox.Show("Time Up!!!");
+            }
+        }
+
+        private int m_currentPage = 0;
+        private int m_totalPages = 1;
+        private int m_questionsPerPage = 10;
+        private System.Timers.Timer m_timer;
+
+
+        struct QuestionControls {
+            public Label label;
+            public RichTextBox question, optiona, optionb, optionc, optiond;
+            public RadioButton selecta, selectb, selectc, selectd;
+            public Panel panel;
+
+            public void ChangeParent()
+            {
+                label.Parent = question.Parent = optiona.Parent = optionb.Parent =
+                    optionc.Parent = optiond.Parent =
+                    selecta.Parent = selectb.Parent = selectc.Parent = selectd.Parent =
+                    panel;
+            }
+        }
+
+        private static String GetRtf(String str)
+        {
+            RichTextBox rtf = new RichTextBox();
+            rtf.Text = str;
+            return rtf.Rtf;
+        }
+        [Serializable]
+        private class Question {
+            public String question=GetRtf("hello"), optiona="", optionb="", optionc="", optiond="";
+        }
+
+        [Serializable]
+        public class Passage
+        {
+            public int passageQuestion = -1;
+            public String passageText="";
+        }
+
+        private List<QuestionControls> m_questionControls =  new List<QuestionControls>();
+        private List<Question> m_questions = new List<Question>();
+        public Passage m_passage = new Passage();
+
+        public RichTextBox m_lastTextBox;
+
+        public void LoadQuestions(String fileName)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            try
+            {
+                using (System.IO.Stream stream = System.IO.File.Open(fileName, System.IO.FileMode.Open))
+                {
+                    var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    m_questions = (List<Question>)bformatter.Deserialize(stream);
+                }
+                using (System.IO.Stream stream = System.IO.File.Open(fileName+"p", System.IO.FileMode.Open))
+                {
+                    Passage p = new Passage();
+                    var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    m_passage = (Passage)bformatter.Deserialize(stream);
+                }
+            }
+            catch (Exception e)
+            {
+                Cursor.Current = Cursors.Default;
+                MessageBox.Show("failed to load the file \"" + fileName + "\"\n\r" + e.Message, "Question Editor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            m_currentPage = 0;
+            m_totalPages = m_questions.Count / m_questionsPerPage;
+            this.RefreshQuestions();
+            Cursor.Current = Cursors.Default;
+        }
+
+        public void RefreshQuestions()
+        {
+            // Remove all questions that are empty
+            m_questions.RemoveAll(q => q.question == "");
+
+            int i = m_currentPage * m_questionsPerPage;
+            foreach (QuestionControls qc in m_questionControls) 
+            {
+                if (i < m_questions.Count)
+                {
+                    Question q = m_questions[i];
+                    qc.label.Text = (i + 1) + ".";
+                    qc.question.Rtf = q.question;
+                    qc.optiona.Rtf = q.optiona;
+                    qc.optionb.Rtf = q.optionb;
+                    qc.optionc.Rtf = q.optionc;
+                    qc.optiond.Rtf = q.optiond;
+                }
+                else
+                    qc.panel.Hide();
+                i++;
+            }
+        }
+
+        public void RefreshControls()
+        {
+            int top = 5;
+            foreach (QuestionControls qc in m_questionControls)
+            {
+                qc.panel.Top = top;
+                qc.question.Top = 5;
+                qc.optiona.Top = qc.optionb.Top = qc.selecta.Top = qc.selectb.Top
+                    = qc.question.Top + qc.question.Height + 10;
+                qc.optionc.Top = qc.optiond.Top = qc.selectc.Top = qc.selectd.Top
+                    = qc.optiona.Top + Math.Max(qc.optiona.Height, qc.optionb.Height) + 10;
+
+                qc.panel.Height = qc.optionc.Top + Math.Max(qc.optiona.Height, qc.optionb.Height) + 20;
+                top += qc.panel.Height + 10;
+            }
+        }
+
+        private void btn_prev_Click(object sender, EventArgs e)
+        {
+            if (m_currentPage == 0)
+                return;
+            m_currentPage--;
+            RefreshQuestions();
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            if (m_currentPage >= m_totalPages - 1)
+                return;
+            m_currentPage++;
+            RefreshQuestions();
+        }
+
+
+        private void content_resized(object _sender, ContentsResizedEventArgs e)
+        {
+            RichTextBox sender = (RichTextBox)_sender;
+            sender.Height = e.NewRectangle.Height + 5;
+            RefreshControls();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
---
+++ IoE Model Entrance/Model Entrance/QuestionPage.designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Model_Entrance
+{
+    partial class QuestionPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuestionPage));
+            this.pnl_holder = new System.Windows.Forms.Panel();
+            this.pnl_question = new System.Windows.Forms.Panel();
+            this.radio_d = new System.Windows.Forms.RadioButton();
+            this.radio_b = new System.Windows.Forms.RadioButton();
+            this.radio_c = new System.Windows.Forms.RadioButton();
+            this.radio_a = new System.Windows.Forms.RadioButton();
+            this.lbl_qn1 = new System.Windows.Forms.Label();
+            this.rtb_question1 = new System.Windows.Forms.RichTextBox();
+            this.rtb_optiona1 = new System.Windows.Forms.RichTextBox();
+            this.rtb_optionb1 = new System.Windows.Forms.RichTextBox();
+            this.rtb_optionc1 = new System.Windows.Forms.RichTextBox();
+            this.rtb_optiond1 = new System.Windows.Forms.RichTextBox();
+            this.pnl_bottom = new System.Windows.Forms.Panel();
+            this.lbl_timer = new System.Windows.Forms.Label();
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnl_holder.SuspendLayout();
+            this.pnl_question.SuspendLayout();
+            this.pnl_bottom.SuspendLayout();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_holder
+            // 
+            this.pnl_holder.AutoScroll = true;
+            this.pnl_holder.Controls.Add(this.pnl_question);
+            this.pnl_holder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_holder.Location = new System.Drawing.Point(0, 24);
+            this.pnl_holder.Name = "pnl_holder";
+            this.pnl_holder.Size = new System.Drawing.Size(900, 426);
+            this.pnl_holder.TabIndex = 0;
+            // 
+            // pnl_question
+            // 
+            this.pnl_question.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pnl_question.AutoSize = true;
+            this.pnl_question.Controls.Add(this.radio_d);
+            this.pnl_question.Controls.Add(this.radio_b);
+            this.pnl_question.Controls.Add(this.radio_c);
+            this.pnl_question.Controls.Add(this.radio_a);
+            this.pnl_question.Controls.Add(this.lbl_qn1);
+            this.pnl_question.Controls.Add(this.rtb_question1);
+            this.pnl_question.Controls.Add(this.rtb_optiona1);
+            this.pnl_question.Controls.Add(this.rtb_optionb1);
+            this.pnl_question.Controls.Add(this.rtb_optionc1);
+            this.pnl_question.Controls.Add(this.rtb_optiond1);
+            this.pnl_question.Location = new System.Drawing.Point(42, 3);
+            this.pnl_question.Name = "pnl_question";
+            this.pnl_question.Size = new System.Drawing.Size(773, 309);
+            this.pnl_question.TabIndex = 2;
+            // 
+            // radio_d
+            // 
+            this.radio_d.AutoSize = true;
+            this.radio_d.Location = new System.Drawing.Point(441, 259);
+            this.radio_d.Name = "radio_d";
+            this.radio_d.Size = new System.Drawing.Size(14, 13);
+            this.radio_d.TabIndex = 1;
+            this.radio_d.TabStop = true;
+            this.radio_d.UseVisualStyleBackColor = true;
+            // 
+            // radio_b
+            // 
+            this.radio_b.AutoSize = true;
+            this.radio_b.Location = new System.Drawing.Point(441, 200);
+            this.radio_b.Name = "radio_b";
+            this.radio_b.Size = new System.Drawing.Size(14, 13);
+            this.radio_b.TabIndex = 1;
+            this.radio_b.TabStop = true;
+            this.radio_b.UseVisualStyleBackColor = true;
+            // 
+            // radio_c
+            // 
+            this.radio_c.AutoSize = true;
+            this.radio_c.Location = new System.Drawing.Point(80, 259);
+            this.radio_c.Name = "radio_c";
+            this.radio_c.Size = new System.Drawing.Size(14, 13);
+            this.radio_c.TabIndex = 1;
+            this.radio_c.TabStop = true;
+            this.radio_c.UseVisualStyleBackColor = true;
+            // 
+            // radio_a
+            // 
+            this.radio_a.AutoSize = true;
+            this.radio_a.Location = new System.Drawing.Point(80, 200);
+            this.radio_a.Name = "radio_a";
+            this.radio_a.Size = new System.Drawing.Size(14, 13);
+            this.radio_a.TabIndex = 1;
+            this.radio_a.TabStop = true;
+            this.radio_a.UseVisualStyleBackColor = true;
+            // 
+            // lbl_qn1
+            // 
+            this.lbl_qn1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbl_qn1.AutoSize = true;
+            this.lbl_qn1.Location = new System.Drawing.Point(8, 15);
+            this.lbl_qn1.Name = "lbl_qn1";
+            this.lbl_qn1.Size = new System.Drawing.Size(16, 13);
+            this.lbl_qn1.TabIndex = 0;
+            this.lbl_qn1.Text = "1.";
+            // 
+            // rtb_question1
+            // 
+            this.rtb_question1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_question1.BackColor = System.Drawing.SystemColors.Window;
+            this.rtb_question1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_question1.Location = new System.Drawing.Point(58, 15);
+            this.rtb_question1.Name = "rtb_question1";
+            this.rtb_question1.ReadOnly = true;
+            this.rtb_question1.Size = new System.Drawing.Size(700, 150);
+            this.rtb_question1.TabIndex = 0;
+            this.rtb_question1.Tag = "qn0";
+            this.rtb_question1.Text = "";
+            // 
+            // rtb_optiona1
+            // 
+            this.rtb_optiona1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_optiona1.BackColor = System.Drawing.SystemColors.Window;
+            this.rtb_optiona1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_optiona1.Location = new System.Drawing.Point(117, 180);
+            this.rtb_optiona1.Name = "rtb_optiona1";
+            this.rtb_optiona1.ReadOnly = true;
+            this.rtb_optiona1.Size = new System.Drawing.Size(281, 50);
+            this.rtb_optiona1.TabIndex = 0;
+            this.rtb_optiona1.Tag = "oa0";
+            this.rtb_optiona1.Text = "";
+            // 
+            // rtb_optionb1
+            // 
+            this.rtb_optionb1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_optionb1.BackColor = System.Drawing.SystemColors.Window;
+            this.rtb_optionb1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_optionb1.Location = new System.Drawing.Point(477, 180);
+            this.rtb_optionb1.Name = "rtb_optionb1";
+            this.rtb_optionb1.ReadOnly = true;
+            this.rtb_optionb1.Size = new System.Drawing.Size(281, 50);
+            this.rtb_optionb1.TabIndex = 0;
+            this.rtb_optionb1.Tag = "ob0";
+            this.rtb_optionb1.Text = "";
+            // 
+            // rtb_optionc1
+            // 
+            this.rtb_optionc1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_optionc1.BackColor = System.Drawing.SystemColors.Window;
+            this.rtb_optionc1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_optionc1.Location = new System.Drawing.Point(117, 240);
+            this.rtb_optionc1.Name = "rtb_optionc1";
+            this.rtb_optionc1.ReadOnly = true;
+            this.rtb_optionc1.Size = new System.Drawing.Size(281, 50);
+            this.rtb_optionc1.TabIndex = 0;
+            this.rtb_optionc1.Tag = "oc0";
+            this.rtb_optionc1.Text = "";
+            // 
+            // rtb_optiond1
+            // 
+            this.rtb_optiond1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rtb_optiond1.BackColor = System.Drawing.SystemColors.Window;
+            this.rtb_optiond1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_optiond1.Location = new System.Drawing.Point(477, 240);
+            this.rtb_optiond1.Name = "rtb_optiond1";
+            this.rtb_optiond1.ReadOnly = true;
+            this.rtb_optiond1.Size = new System.Drawing.Size(281, 50);
+            this.rtb_optiond1.TabIndex = 0;
+            this.rtb_optiond1.Tag = "od0";
+            this.rtb_optiond1.Text = "";
+            // 
+            // pnl_bottom
+            // 
+            this.pnl_bottom.BackColor = System.Drawing.Color.LightGray;
+            this.pnl_bottom.Controls.Add(this.lbl_timer);
+            this.pnl_bottom.Controls.Add(this.btn_prev);
+            this.pnl_bottom.Controls.Add(this.btn_next);
+            this.pnl_bottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnl_bottom.Location = new System.Drawing.Point(0, 450);
+            this.pnl_bottom.Name = "pnl_bottom";
+            this.pnl_bottom.Size = new System.Drawing.Size(900, 50);
+            this.pnl_bottom.TabIndex = 1;
+            // 
+            // lbl_timer
+            // 
+            this.lbl_timer.AutoSize = true;
+            this.lbl_timer.Location = new System.Drawing.Point(50, 27);
+            this.lbl_timer.Name = "lbl_timer";
+            this.lbl_timer.Size = new System.Drawing.Size(119, 13);
+            this.lbl_timer.TabIndex = 2;
+            this.lbl_timer.Text = "Time Elapsed: 00:00:00";
+            // 
+            // btn_prev
+            // 
+            this.btn_prev.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_prev.Location = new System.Drawing.Point(620, 10);
+            this.btn_prev.Name = "btn_prev";
+            this.btn_prev.Size = new System.Drawing.Size(100, 30);
+            this.btn_prev.TabIndex = 1;
+            this.btn_prev.Text = "← Previous";
+            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_next.Location = new System.Drawing.Point(750, 10);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(100, 30);
+            this.btn_next.TabIndex = 1;
+            this.btn_next.Text = "Next →";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "qset";
+            this.openFileDialog.Filter = "Question set file|*.qset";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "qset";
+            this.saveFileDialog.Filter = "Question set file|*.qset";
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(900, 24);
+            this.mainMenu.TabIndex = 3;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // QuestionPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 500);
+            this.Controls.Add(this.pnl_holder);
+            this.Controls.Add(this.pnl_bottom);
+            this.Controls.Add(this.mainMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mainMenu;
+            this.MinimumSize = new System.Drawing.Size(900, 400);
+            this.Name = "QuestionPage";
+            this.Text = "IoE Model Entrance - Question Editor";
+            this.Load += new System.EventHandler(this.QuestionsEditor_Load);
+            this.pnl_holder.ResumeLayout(false);
+            this.pnl_holder.PerformLayout();
+            this.pnl_question.ResumeLayout(false);
+            this.pnl_question.PerformLayout();
+            this.pnl_bottom.ResumeLayout(false);
+            this.pnl_bottom.PerformLayout();
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_holder;
+        private System.Windows.Forms.Label lbl_qn1;
+        private System.Windows.Forms.RichTextBox rtb_optionb1;
+        private System.Windows.Forms.RichTextBox rtb_optiond1;
+        private System.Windows.Forms.RichTextBox rtb_optiona1;
+        private System.Windows.Forms.RichTextBox rtb_question1;
+        private System.Windows.Forms.RichTextBox rtb_optionc1;
+        private System.Windows.Forms.Panel pnl_bottom;
+        private System.Windows.Forms.Button btn_prev;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Panel pnl_question;
+        private System.Windows.Forms.RadioButton radio_d;
+        private System.Windows.Forms.RadioButton radio_b;
+        private System.Windows.Forms.RadioButton radio_c;
+        private System.Windows.Forms.RadioButton radio_a;
+        private System.Windows.Forms.Label lbl_timer;
+    }
+}
+
